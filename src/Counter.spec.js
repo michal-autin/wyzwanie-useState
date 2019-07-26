c8b7518eab30dda740003ac7774ec5f1c7709041
michal izydorski <michalefg@gmail.com>
2019-07-26 14:59:44 +0200

    another update

--- src/Counter.spec.js
+++ src/Counter.spec.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Counter from './Counter'
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import Counter from "./Counter";
 
-it('should increment a counter', () => {
-  const el = document.createElement('div')
-  document.body.appendChild(el)
+it("should increment a counter", () => {
+  const el = document.createElement("div");
+  document.body.appendChild(el);
   // we attach the element to document.body to ensure events work
-  ReactDOM.render(<Counter />, el)
-  const button = document.querySelector('button')
-  const pElement = document.querySelector('p')
-  console.log(button.innerHTML)
-  for (let i = 0; i < 3; i++) {
-    button.dispatchEvent(new MouseEvent('click', { bubbles: true }))
-  }
-  expect(pElement.innerHTML).toBe('Wartość licznika: 3')
-})
+  act(() => {
+    ReactDOM.render(<Counter />, el);
+  });
+  const button = document.querySelector("button");
+  const pElement = document.querySelector("p");
+  console.log(button.innerHTML);
+  act(() => {
+    for (let i = 0; i < 3; i++) {
+      button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    }
+  });
+  expect(pElement.innerHTML).toBe("Wartość licznika: 3");
+});
