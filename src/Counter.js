7b8455d8eea156cd0b62b8b8e6bb40821d1e2f75
michal-autin <michalefg@gmail.com>
2019-07-26 14:52:51 +0200

    updated test

--- src/Counter.js
+++ src/Counter.js
@@ -10,9 +10,6 @@ export default function Counter() {
   const decrease = () => {
     setValue(s => s - 1)
   }
-  useEffect(() => {
-    ;[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => increase())
-  }, [])
 
   return (
     <div>
